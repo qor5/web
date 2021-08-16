18c47bcb907b065c4aa839ada1a2d9717b3f28fe
sunfmin <sunfmin@gmail.com>
2021-08-16 13:46:41 +0800

    Add fieldname, change to use npm for github actions

--- corejs/src/fieldname.ts
+++ corejs/src/fieldname.ts
@@ -1,29 +1,31 @@
 import {VNode, VNodeDirective} from "vue";
 import {DirectiveBinding} from "vue/types/options";
+import {setFormValue} from "@/utils";
 
 
 export function fieldNameDirective(form: FormData) {
-	let onInput: EventListener;
+	let onInput: EventListener
+	let onComponentInput: Function
 
 	function setValue(target: HTMLInputElement, fieldName: string) {
 		// console.log("target.value", target.value, "target.type", target.type, "target.checked", target.checked, target.form)
 		if (target.files) {
-			form.delete(fieldName);
+			form.delete(fieldName)
 			for (const f of target.files) {
-				form.append(fieldName, f, f.name);
+				form.append(fieldName, f, f.name)
 			}
 		} else if (target.type === 'checkbox') {
 			if (target.checked) {
-				form.set(fieldName, target.value);
+				form.set(fieldName, target.value)
 			} else {
-				form.delete(fieldName);
+				form.delete(fieldName)
 			}
 		} else if (target.type === 'radio') {
 			if (target.checked) {
-				form.set(fieldName, target.value);
+				form.set(fieldName, target.value)
 			}
 		} else {
-			form.set(fieldName, target.value);
+			form.set(fieldName, target.value)
 		}
 	}
 
@@ -37,27 +39,54 @@ export function fieldNameDirective(form: FormData) {
 		}
 	}
 
-	function bind(el: HTMLElement, binding: DirectiveBinding, vnode: VNode) {
-		// console.log("vnode.context", vnode.context)
-		const fieldName = binding.value
-		setValue(el as HTMLInputElement, fieldName)
-		onInput = inputEventHandler(fieldName)
-		el.addEventListener("input", onInput)
 
+	function removeListeners(el: HTMLElement, vnode: VNode) {
+		if (vnode.componentInstance) {
+			const comp = vnode.componentInstance
+			if(onComponentInput) {
+				comp.$off("input", onComponentInput)
+			}
+		} else {
+			if(onInput) {
+				el.removeEventListener("input", onInput)
+			}
+		}
 	}
 
-	function unbind(el: HTMLElement) {
-		el.removeEventListener("input", onInput)
+	function setListeners(el: HTMLElement, vnode: VNode, fieldName: string) {
+		if (vnode.componentInstance) {
+			const comp = vnode.componentInstance
+			if(onComponentInput) {
+				comp.$off("input", onComponentInput)
+			}
+			setFormValue(form, fieldName, comp.$attrs["input-value"] || comp.$attrs["value"])
+			onComponentInput = (values: any) => {
+				setFormValue(form, fieldName, values);
+			}
+			vnode.componentInstance.$on("input", onComponentInput)
+		} else {
+			if(onInput) {
+				el.removeEventListener("input", onInput)
+			}
+			setValue(el as HTMLInputElement, fieldName)
+			onInput = inputEventHandler(fieldName)
+			el.addEventListener("input", onInput)
+		}
 	}
 
-	function update(el: HTMLElement, binding: VNodeDirective) {
-		el.removeEventListener("input", onInput)
-		onInput = inputEventHandler(binding.value)
-		el.addEventListener("input", onInput)
+	function inserted(el: HTMLElement, binding: DirectiveBinding, vnode: VNode) {
+		setListeners(el, vnode, binding.value)
 	}
 
+	function update(el: HTMLElement, binding: VNodeDirective, vnode: VNode) {
+		setListeners(el, vnode, binding.value)
+	}
+
+	function unbind(el: HTMLElement, binding: DirectiveBinding, vnode: VNode) {
+		removeListeners(el, vnode)
+	}
 	return {
-		bind,
+		inserted,
 		unbind,
 		update,
 	}
