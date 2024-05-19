a4c8e888ca9cec3c488b297b6eea9a798fa3ded6
Felix Sun <sunfmin@gmail.com>
2024-05-20 05:22:01 +0800

    Add more helper expect tools

--- multipartestutils/case.go
+++ multipartestutils/case.go
@@ -6,15 +6,18 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
+	"strings"
 	"testing"
 )
 
 type TestCase struct {
-	Name               string
-	ReqFunc            func() *http.Request
-	EventResponseMatch func(t *testing.T, er *TestEventResponse)
-	PageMatch          func(t *testing.T, body *bytes.Buffer)
-	Debug              bool
+	Name                        string
+	ReqFunc                     func() *http.Request
+	EventResponseMatch          func(t *testing.T, er *TestEventResponse)
+	PageMatch                   func(t *testing.T, body *bytes.Buffer)
+	Debug                       bool
+	ExpectPageBodyContains      []string
+	ExpectPortalUpdate0Contains []string
 }
 
 type TestPortalUpdate struct {
@@ -45,16 +48,39 @@ func RunCase(t *testing.T, c TestCase, handler http.Handler) {
 		t.Log("======== Response ========")
 		t.Log(w.Body.String())
 	}
+
 	if c.EventResponseMatch != nil {
 		var er TestEventResponse
 		err := json.NewDecoder(w.Body).Decode(&er)
 		if err != nil {
-			t.Fatalf("%s for: %#+v", err, w.Body.String())
+			t.Errorf("%s for: %s", err, w.Body.String())
 		}
 		c.EventResponseMatch(t, &er)
+	}
+	if len(c.ExpectPortalUpdate0Contains) > 0 {
+		var er TestEventResponse
+		err := json.NewDecoder(w.Body).Decode(&er)
+		if err != nil {
+			t.Errorf("%s for: %s", err, w.Body.String())
+		}
+		if len(er.UpdatePortals) == 0 {
+			t.Errorf("No UpdatePortals in : %#+v", er)
+		}
+		for _, u := range c.ExpectPortalUpdate0Contains {
+			if !strings.Contains(er.UpdatePortals[0].Body, u) {
+				t.Errorf("portal %s doesn't contains: %s", er.UpdatePortals[0].Body, u)
+			}
+		}
 	}
 
 	if c.PageMatch != nil {
 		c.PageMatch(t, w.Body)
+	}
+	if len(c.ExpectPageBodyContains) > 0 {
+		for _, v := range c.ExpectPageBodyContains {
+			if !strings.Contains(w.Body.String(), v) {
+				t.Errorf("page body %s doesn't contains: %s", w.Body.String(), v)
+			}
+		}
 	}
 }
