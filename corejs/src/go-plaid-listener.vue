b728fd181969cc4097d2f56d5f55fb078d550fb6
molon <3739161+molon@users.noreply.github.com>
2024-07-03 00:49:59 +0800

    resolve listeners on duplicate execution issues

--- corejs/src/go-plaid-listener.vue
+++ corejs/src/go-plaid-listener.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import { inject, useAttrs } from 'vue'
+import { inject, useAttrs, onMounted, onUnmounted } from 'vue'
 defineOptions({
   inheritAttrs: false
 })
@@ -11,9 +11,22 @@ const vars: any = inject('vars')
 const emitter = vars.__emitter
 const attrs = useAttrs()
 
-Object.keys(attrs).forEach((key) => {
-  if (key.startsWith('on')) {
-    emitter.on(key.slice(2), attrs[key])
-  }
+const handlers: Record<string, Function> = {}
+
+onMounted(() => {
+  Object.keys(attrs).forEach((key) => {
+    if (key.startsWith('on')) {
+      const eventName = key.slice(2)
+      const handler = attrs[key] as Function
+      handlers[eventName] = handler
+      emitter.on(eventName, handler)
+    }
+  })
+})
+
+onUnmounted(() => {
+  Object.keys(handlers).forEach((eventName) => {
+    emitter.off(eventName, handlers[eventName])
+  })
 })
 </script>
