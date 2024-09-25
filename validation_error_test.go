f4b6b1d3c8131b20a1560ba05fab8948397e4939
TerryZhou <terryZhou@theplant.jp>
2024-09-25 11:07:07 +0800

    Remove useless

--- validation_error_test.go
+++ validation_error_test.go
@@ -12,9 +12,6 @@ func TestValidationGlobalError(t *testing.T) {
 	errText := "test validation global error "
 	err := errors.New(errText)
 	validationErr := web.ValidationGlobalError(err)
-	if !reflect.DeepEqual(validationErr.Error(), err.Error()) {
-		t.Fail()
-	}
 	if validationErr.Error() != errText {
 		t.Fail()
 	}
